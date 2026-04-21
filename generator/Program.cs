f074527950803e75df2684d9de0de7a50d365b83
Paul Chen <lancevdance@gmail.com>
2026-04-21 16:23:27 +0800

    Copy files.

--- generator/Program.cs
+++ generator/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace OriginLab.DocumentGeneration;
+﻿using System.Xml.Linq;
+
+namespace OriginLab.DocumentGeneration;
 
 class Program
 {
-    static void Main(string[] args)
+    static int Main(string[] args)
     {
+        var outputPath = args[0];
 
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+
+        if (Path.GetDirectoryName(outputPath) is not string targetBookPath)
+            return -2;
+
+        var languages = (from subPath in Directory.EnumerateDirectories(targetBookPath)
+                         let name = Path.GetFileName(subPath)
+                         where name.Length == 2
+                         select name).ToArray();
+
+        if (!languages.Contains("en"))
+            return -3;
+
+        var bookXmlPath = Path.Combine(targetBookPath, "en", "book.xml");
+
+        if (!File.Exists(bookXmlPath))
+            return -4;
+
+        var bookXml = XElement.Load(bookXmlPath);
+
+        foreach (var lang in languages)
+        {
+            var srcDir = Path.Combine(targetBookPath, lang);
+            var dstDir = Directory.CreateDirectory(Path.Combine(outputPath, lang));
+
+            foreach (var page in bookXml.Descendants("page"))
+            {
+                var dir = dstDir.FullName;
+                var url = page.Attribute("url")!.Value;
+                var sep = url.IndexOf('/');
+                if (sep > 0)
+                {
+                    dir = Path.Combine(dir, url[(sep + 1)..]);
+                    Directory.CreateDirectory(dir);
+                }
+
+                var srcFilePath = Path.Combine(srcDir, page.Attribute("file")!.Value);
+                var dstFilePath = Path.Combine(dir, "index.html");
+
+                File.Copy(srcFilePath, dstFilePath);
+            }
+        }
+
+        return 0;
     }
 }
